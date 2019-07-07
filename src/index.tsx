12ec113f099a57d038ab34f5e14996105ec6ec75
Dmitry Guzeev <dmitry.guzeev@yahoo.com>
2019-07-07 14:16:29 +0300

    Refactor index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,28 @@
-/**
- * @file index.tsx
- * @author Dmitry Guzeev <dmitry.guzeev@yahoo.com>
- */
+import React from "react";
+import ReactDOM from "react-dom";
+import store from "./store";
+import history from "+/history";
+import AppWrapper from "+/components/AppWrapper";
+import App from "./components/App";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import "+/global_styles.scss";
+import "+/webkit_scrollbar.scss";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import store, { history as storeHistory } from './store';
-import AppWrapper from './components/AppWrapper';
-import App from './components/App';
+// Setup font awesome icons
+library.add(fab, fas);
 
 // Create the application container element and append it to the document body.
-let appContainerElement = document.createElement('div');
-appContainerElement.style.height = '100%';
-appContainerElement.id = 'root';
+let appContainerElement = document.createElement("div");
+appContainerElement.style.height = "100%";
+appContainerElement.id = "root";
 document.body.appendChild(appContainerElement);
 
 // Render the application onto the root element
 ReactDOM.render(
-  <AppWrapper store={store} history={storeHistory}>
+  <AppWrapper store={store} history={history}>
     <App />
   </AppWrapper>,
-  appContainerElement,
+  appContainerElement
 );
