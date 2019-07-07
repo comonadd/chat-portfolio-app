8739435908d6f4a32daa2eacb6703de1017ec27b
Dmitry Guzeev <dmitry.guzeev@yahoo.com>
2019-07-07 14:30:04 +0300

    Refactor Loader component

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,32 +1,29 @@
-/**
- * @file index.tsx
- * @author Dmitry Guzeev <dmitry.guzeev@yahoo.com>
- */
-
-import React from 'react';
-import classnames from 'classnames';
-
-const style = require('./style');
+import React from "react";
+import classnames from "classnames";
+import "./style.scss";
 
 export interface LoaderItemProps {
   index: number;
 }
 
-const LoaderItem = (props: LoaderItemProps) =>
+const LoaderItem = (props: LoaderItemProps) => (
   <li
-    className={classnames(style.loader__items__item, [
-        'style.loader__items__item_' + props.index,
+    className={classnames("loader__items__item", [
+      `loader__items__item_${props.index}`
     ])}
-  />;
+  />
+);
 
-const LoaderItems = () =>
-  <ul className={style.loader__items}>
-    {[0, 1, 2, 3, 4].map((i) => <LoaderItem key={i} index={i} />)}
-  </ul>;
+const LoaderItems = () => (
+  <ul className={"loader__items"}>
+    {[0, 1, 2, 3, 4].map(i => (
+      <LoaderItem key={i} index={i} />
+    ))}
+  </ul>
+);
 
-const Loader = () =>
-  <div className={style.loader}>
+export default () => (
+  <div className={"loader"}>
     <LoaderItems />
-  </div>;
-
-export default Loader;
+  </div>
+);
